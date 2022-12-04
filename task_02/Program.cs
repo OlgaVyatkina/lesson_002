8d872b6eb8ee4100a8d12b24860b1b7100400146
Olga Vyatkina <calisto999@mail.ru>
2022-12-04 19:34:55 +0300

    add tasks 1 nd 2

---
+++ task_02/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число:");
+string N = Console.ReadLine() ?? "";
+if ((N.Length-1) < 2) {
+  Console.WriteLine("Третьей цифры нет");  
+}
+else {
+    Console.WriteLine(N[2]); 
+}
