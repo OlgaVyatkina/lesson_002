74c4ec1e5af4ae06c60330de0cc92d54984b0abc
Olga Vyatkina <calisto999@mail.ru>
2022-12-04 22:02:56 +0300

    fix

--- task_01/Program.cs
+++ task_01/Program.cs
@@ -2,4 +2,8 @@
 
 Console.WriteLine("Введите число:");
 string N = Console.ReadLine() ?? "";
+if ((N.Length-1) < 2) {
+Console.WriteLine("Число не трехзначное"); }
+else {
 Console.WriteLine(N[1]);
+}
--- task_03/Program.cs
+++ task_03/Program.cs
@@ -2,30 +2,5 @@
 
 Console.WriteLine("Введите число от 1 до 7:");
 int day = Convert.ToInt32(Console.ReadLine());
-switch (day) {
-case 1:
-Console.WriteLine("Понедельник");
-break;
-case 2:
-Console.WriteLine("Вторник");
-break;
-case 3:
-Console.WriteLine("Среда");
-break;
-case 4:
-Console.WriteLine("Четверг");
-break;
-case 5:
-Console.WriteLine("Пятница");
-break;
-case 6:
-Console.WriteLine("Суббота - выходной день!");
-break;
-case 7:
-Console.WriteLine("Воскресенье - выходной день!");
-break;
-
-default:
-Console.WriteLine("Таких дней нет");
-break;
-}
+if(day > 0 && day < 6) {Console.WriteLine("Не выходной )");}
+else {Console.WriteLine("Выходной день!");}
