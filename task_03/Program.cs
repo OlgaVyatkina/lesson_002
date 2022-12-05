8330231e0ad9776ffa0098d17ae5a54a4c3f9f62
Olga Vyatkina <calisto999@mail.ru>
2022-12-05 22:40:39 +0300

    task3 fix

--- task_03/Program.cs
+++ task_03/Program.cs
@@ -2,6 +2,10 @@
 
 Console.WriteLine("Введите число от 1 до 7:");
 int day = Convert.ToInt32(Console.ReadLine());
-if (day == 6 || day == 7) {Console.WriteLine("Выходной день");}
-else if (day <= 5) {Console.WriteLine("Не выходной");}
-else if (day > 7) {Console.WriteLine("Это не день недели");}
+if (day >= 1 && day <= 7) {
+    string result = (day == 6 || day == 7) ? "Выходной день" : "Не выходной"; 
+    Console.WriteLine(result);
+    }
+else {
+    Console.WriteLine("Это не день недели");
+    }
